ece371674f687d434d6f1914bd562688a34470e7
Jialing <bquantumwill@gmail.com>
2025-04-08 12:10:55 +0800

    adjust

--- vocab.py
+++ vocab.py
@@ -1,28 +1,4 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
-from sae.sae import Sae
-from sae.config import *
-from safetensors.torch import load_model
-from pathlib import Path
-from sklearn.metrics import f1_score, accuracy_score, recall_score
-from collections import Counter
-
-sae_name = "EleutherAI/sae-pythia-160m-32k"
-# sae-pythia-410m-65k
-saes = Sae.load_many("EleutherAI/sae-pythia-160m-32k")
-sae = Sae.load_from_hub(sae_name, hookpoint="layers.11")
-
-# sae = ConvSae(768, ConvSaeConfig(kernel_size=7))
-# device="cpu"
-# path = Path("sae-ckpts/gpt_neox.layers.11")
-
-# load_model(
-#     model=sae,
-#     filename=str(path / "sae.safetensors"),
-#     device=str(device),
-#     # TODO: Maybe be more fine-grained about this in the future?
-#     strict=True,
-# )
 
 model_name = "EleutherAI/pythia-410m"
 # 410m
@@ -36,3 +12,4 @@ vocab = sorted(vocab, key=lambda x: x[1])
 with open("vocab_list_410m.txt", "w", encoding="utf-8") as f:
     for token, idx in vocab:
         f.write(f"Token: {token} -> ID: {idx}\n")
+print("Vocabulary list saved to vocab_list_410m.txt")

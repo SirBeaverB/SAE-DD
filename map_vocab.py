f5967a1b595ca19e9fb304e8942ee1a394d3eecb
SirBeaver <bquantumwill@gmail.com>
2025-04-08 14:40:19 +0000

    map adjusted

--- map_vocab.py
+++ map_vocab.py
@@ -4,17 +4,17 @@ from sae.sae import Sae
 from sae.config import *
 from safetensors.torch import load_model
 from pathlib import Path
-from sklearn.metrics import f1_score, accuracy_score, recall_score
 from collections import Counter
 import json
+from tqdm import tqdm
 
-sae_name = "EleutherAI/sae-pythia-160m-32k"
+sae_name = "EleutherAI/sae-pythia-410m-65k"
 # sae-pythia-410m-65k
-saes = Sae.load_many("EleutherAI/sae-pythia-160m-32k")
-sae = Sae.load_from_hub(sae_name, hookpoint="layers.11")
+saes = Sae.load_many("EleutherAI/sae-pythia-410m-65k")
+sae = Sae.load_from_hub(sae_name, hookpoint="layers.11.mlp")
 
 
-model_name = "EleutherAI/pythia-160m"
+model_name = "EleutherAI/pythia-410m"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
@@ -39,25 +39,31 @@ print("token_list loaded.")
 model = AutoModelForCausalLM.from_pretrained(model_name)
 embs = []
 with torch.inference_mode(): # no gradient
-    for token in token_list[:10]: #test
+    for token in tqdm(token_list, desc="Processing tokens"):
         inputs = tokenizer(token, return_tensors="pt")
         outputs = model(**inputs, output_hidden_states=True)
-        hidden_states = outputs.hidden_states[-1] # get last layer
-        latent_acts = sae.encode(hidden_states) # put into SAE
+        hidden_states = outputs.hidden_states[-1]  # get last layer
+        latent_acts = sae.encode(hidden_states)  # put into SAE
         latent_features_sum = torch.zeros(sae.encoder.out_features).to(sae.encoder.weight.device)
-        latent_features_sum[latent_acts.top_indices.flatten()] += latent_acts.top_acts.flatten() # sum up the latent features
-        latent_features_sum /= hidden_states.numel() / hidden_states.shape[-1] # average
+        latent_features_sum[latent_acts.top_indices.flatten()] += latent_acts.top_acts.flatten()  # sum up the latent features
+        latent_features_sum /= hidden_states.numel() / hidden_states.shape[-1]  # average
         topk = latent_features_sum.topk(k=32)
-        embs.append(list(zip(topk.indices.tolist(), topk.values.tolist()))) # save indices and scores as tuples
+        embs.append(list(zip(topk.indices.tolist(), topk.values.tolist())))  # save indices and scores as tuples
+print("embeddings calculated.")
 
 embs = [set(i) for i in embs]
 embs_with_tokens = []
-for idx, (token, emb_set) in enumerate(zip(token_list[:10], embs)):  # Use the same slice as above
+for idx, (token, emb_set) in enumerate(zip(token_list, embs)):  # Use the same slice as above
     embs_with_tokens.append({
         "index": idx,
         "token": token,
         "embeddings": sorted(list(emb_set), key=lambda x: x[1], reverse=True)
     })
+print("embeddings with tokens created.")
 
-with open("embeddings_with_tokens.json", "w", encoding="utf-8") as f:
-    json.dump(embs_with_tokens, f, ensure_ascii=False, indent=4)
+chunk_size = 5000
+for i in range(0, len(embs_with_tokens), chunk_size):
+    chunk = embs_with_tokens[i:i + chunk_size]
+    file_name = f"embeddings_with_token/embeddings_with_tokens_part_{i // chunk_size + 1}.json"
+    with open(file_name, "w", encoding="utf-8") as f:
+        json.dump(chunk, f, ensure_ascii=False, indent=4)
